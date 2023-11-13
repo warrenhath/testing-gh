6e96cc426472a86ac49f96c077052c4a5142a557
warrenhath <warrenhath@gmail>
2023-11-13 13:13:29 -0500

    fixed a bug

--- fcopy.py
+++ fcopy.py
@@ -1,11 +1,11 @@
 import sys
 
 def fcopy(file1, file2):
-    with open(file1, "rt"):
-        with open(file2, "wt"):
-            l1 = file1.readlines()
+    with open(file1, "rt") as f: 
+        with open(file2, "wt") as f_new:
+            l1 = f.readlines()
             for line in l1:
-                file2.write(line)
+                f_new.write(line)
 
 def main():
     if sys.argv[0] == "fcopy.py":
